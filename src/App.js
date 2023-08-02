2fecda40f713982e9b280f32c2e350e7ba71d9e2
Abhishek Kushwaha <20104@iiitu.ac.in>
2023-08-02 22:51:32 +0530

    profile update 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import Home from "./Components/Home";
-import Login from "./Login.jsx";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }
