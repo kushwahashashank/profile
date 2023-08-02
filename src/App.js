137a50d8fabd3bd58bbd15862c491ad57b527266
Abhishek Kushwaha <20104@iiitu.ac.in>
2023-08-02 22:40:05 +0530

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import Home from "./Components/Home";
+import Login from "./Login.jsx";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
